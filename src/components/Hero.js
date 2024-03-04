c69a91bcdb420a7c2ddec06a1a61ff614e7ae43b
Djon Rikko <djon.rikko@gmail.com>
2024-03-04 17:28:24 +0700

    add logo

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,11 +6,10 @@ import heroImg from '../images/web-dev.svg';
 const Hero = () => {
     return (
         <>
-            <div className="hero" id='hero'>
-                <div>
-                    <NavBar />
-                </div>
-                
+			<div>
+                <NavBar />
+            </div>
+			<div className="hero" id='hero'>
                 <div className="m-auto overflow-hidden mx-4 mt-8 lg:mt-4 p-2 md:p-12 h-5/6" data-aos="zoom-in">
 
                     <div id='hero' className="flex flex-col lg:flex-row py-8 justify-between text-center lg:text-left">
--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -24,7 +24,10 @@ const NavBar = () => {
         <nav className={`fixed top-0 w-full z-30 transition duration-300 ease-in-out mb-16 ${!top && 'bg-white shadow-lg'}`}>
             <div className="flex flex-row justify-between items-center py-2">
                 <div className="flex flex-row justify-center md:px-12 md:mx-12 items-center text-center font-semibold">
-                    <HashLink smooth to="/#hero"><img alt="card img" style={{ width: 80, height: 80 }} resizeMode="cover" className="rounded-t float-right duration-1000 w-full" src={logo} /></HashLink>
+                    <HashLink smooth to="/#hero">
+						<img alt="card img" style={{ width: 60, height: 60 }} resizeMode="cover" className="rounded-t float-right duration-1000 w-full" src={logo} />
+					</HashLink>
+                    
                 </div>
                 <div className="group flex flex-col items-center">
                     <button className="p-2 rounded-lg lg:hidden text-blue-900" onClick={handleClick}>
